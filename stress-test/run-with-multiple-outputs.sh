1ae4682acd5622a54daaf426f42fa48cfdcc971f
Tejaswinidv <43945438+Tejaswinidv@users.noreply.github.com>
2021-12-10 02:36:00 +0530

    Update run-with-multiple-outputs.sh

--- stress-test/run-with-multiple-outputs.sh
+++ stress-test/run-with-multiple-outputs.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-
+value=0
 while read METRIC_VALUE; do
   echo ${METRIC_VALUE}
+  value= ${value} + ${METRIC_VALUE}
   curl -X POST -H "Authorization: Bearer ${WAVEFRONT_API_KEY}" \
   -d "${METRIC_NAME} ${METRIC_VALUE} source=concourse" \
   "https://vmwareprod.wavefront.com/report"
 done < event/metric_value.txt
+echo ${value}
