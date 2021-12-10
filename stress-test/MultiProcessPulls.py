b91ce4ba7fee466c0fedebccb615f1c8c7c688dd
Tejaswinidv <43945438+Tejaswinidv@users.noreply.github.com>
2021-12-10 21:04:07 +0530

    Update MultiProcessPulls.py

--- stress-test/MultiProcessPulls.py
+++ stress-test/MultiProcessPulls.py
@@ -5,6 +5,7 @@ import time
 from multiprocessing import Pool
 import multiprocessing
 import timeit
+import random 
 
 def img_pkg_pull(self):
             #print(sequence);
@@ -12,12 +13,12 @@ def img_pkg_pull(self):
     print("\n Entered Process \n ***PROCESSS ID**** : ",pid)
     tap_list = ["tap-packages:0.3.0-build.2", "tap-packages:0.3.0-build.1", "tap-packages:0.4.0-build.11", "tap-packages:0.4.0-build.10", "tap-packages:0.4.0-build.9"]
     tap_pkg = random.choice(tap_list)
-    #command = ('imgpkg pull -b registry-acceptance.tanzu.vmware.com/tanzu-application-platform/%s -o /tmp/tap/%s' % (tap_pkg,pid) )
-    command = ('imgpkg pull -b registry-acceptance.pivotal.io/tanzu-application-platform/tap-packages:0.3.0-build.2 -o /tmp/tap/%s' % pid )
+    command = ('imgpkg pull -b registry-acceptance.tanzu.vmware.com/tanzu-application-platform/%s -o /tmp/tap/%s' % (tap_pkg,pid) )
+    #command = ('imgpkg pull -b registry-acceptance.pivotal.io/tanzu-application-platform/tap-packages:0.3.0-build.2 -o /tmp/tap/%s' % pid )
     #print ("\nPull Command is : "+command)
     original_time = time.time()
     os.system(command) 
-    print(command) 
+    #print(command) 
     #print(timeit.timeit(setup = os.system(command), stmt = os.system(command), number = 1))
     times_now = time.time() - original_time
     python_file = open("event/metric_value.txt", "a+")
