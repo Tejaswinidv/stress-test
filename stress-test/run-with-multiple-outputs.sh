d45928ac1366fe3cb48673c6196c70d76873b033
Tejaswinidv <43945438+Tejaswinidv@users.noreply.github.com>
2021-12-11 22:46:54 +0530

    Update run-with-multiple-outputs.sh

--- stress-test/run-with-multiple-outputs.sh
+++ stress-test/run-with-multiple-outputs.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 value=0
-apt-get install bc
+
 while read METRIC_VALUE; do
   echo ${METRIC_VALUE}
-  m_value=$(bc -l <<<"${METRIC_VALUE}") 
-  #echo ${m_value}
+  m_value=$((METRIC_VALUE)) 
+  
   curl -X POST -H "Authorization: Bearer ${WAVEFRONT_API_KEY}" \
   -d "${METRIC_NAME} ${m_value} source=concourse" \
   "https://vmwareprod.wavefront.com/report"
 done < event/metric_value.txt
 echo ${value}
-
-awk '{curl -X POST -H "Authorization: Bearer ${WAVEFRONT_API_KEY}" \
-  -d "${METRIC_NAME} $0 source=concourse" \
-  "https://vmwareprod.wavefront.com/report"}END{print $0}' event/metric_value.txt
